55a136910689858c5cea06382d1024a501932dbe
pixel-Teee <1173769234@qq.com>
2022-04-17 18:04:08 +0800

    complete string parser

---
+++ CuteJson/CuteJson/cutejson.h
@@ -0,0 +1,301 @@
+#pragma once
+
+#include <assert.h>
+
+//-----expect c's next character == ch
+#define EXPECT(c, ch) do{ assert(*c->json == (ch)); c->json++; } while(0)
+
+#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
+
+#define cute_init(v) do { (v)->type = CUTE_NULL; } while(0)
+
+#define cute_set_null(v) cute_free(v)
+
+#ifndef CUTE_PARSE_STACK_INIT_SIZE
+#define CUTE_PARSE_STACK_INIT_SIZE 256
+#endif
+
+typedef enum {
+	CUTE_NULL,
+	CUTE_FALSE,
+	CUTE_TRUE,
+	CUTE_NUMBER,
+	CUTE_STRING,
+	CUTE_ARRAY,
+	CUTE_OBJECT
+}cute_type;
+
+typedef struct {
+	union {
+		struct { char* s; size_t len; }s;//string
+		double n;//number
+	}u;
+	cute_type type;
+}cute_value;
+
+typedef struct {
+	const char* json;
+	char* stack;//stack
+	size_t size, top;//stack
+}cute_context;
+
+enum {
+	CUTE_PARSE_OK = 0,
+	CUTE_PARSE_EXPECT_VALUE,
+	CUTE_PARSE_INVALID_VALUE,
+	CUTE_PARSE_NUMBER_TOO_BIG,
+	CUTE_PARSE_MISS_QUOTATION_MARK,
+	CUTE_PARSE_INVALID_STRING_CHAR,
+	CUTE_PARSE_INVALID_STRING_ESCAPE,
+	CUTE_PARSE_ROOT_NOT_STNGULAR
+};
+
+//------free and alloc------
+void cute_free(cute_value* v) {
+	assert(v != NULL);
+	if (v->type == CUTE_STRING)
+		free(v->u.s.s);
+	v->type = CUTE_NULL;
+}
+//------free and alloc------
+
+//------stack push and pop------
+static void* cute_context_push(cute_context* c, size_t size) {
+	void* ret;
+	assert(size > 0);
+	if (c->top + size >= c->size)
+	{
+		if (c->size == 0)
+			c->size = CUTE_PARSE_STACK_INIT_SIZE;
+		while (c->top + size >= c->size)
+			c->size += c->size >> 1;/*c->size * 1.5*/
+		c->stack = (char*)realloc(c->stack, c->size);
+	}
+	ret = c->stack + c->top;
+	c->top += size;
+	return ret;
+}
+
+static void* cute_context_pop(cute_context* c, size_t size) {
+	assert(c->top >= size);
+	return c->stack + (c->top -= size);
+}
+//------stack push and pop------
+
+//------get type------
+cute_type cute_get_type(const cute_value* v) {
+	return v->type;
+}
+//------get type------
+
+//------get set number------
+double cute_get_number(const cute_value* v) {
+	assert(v != NULL && v->type == CUTE_NUMBER);
+	return v->u.n;
+}
+
+void cute_set_number(cute_value* v, double n) {
+	cute_free(v);
+	v->u.n = n;
+	v->type = CUTE_NUMBER;
+}
+//------get set number------
+
+//------get set boolean------
+int cute_get_boolean(const cute_value* v) {
+	assert(v != NULL && (v->type == CUTE_TRUE || v->type == CUTE_FALSE));
+	return v->type == CUTE_TRUE;
+}
+
+void cute_set_boolean(cute_value* v, int b) {
+	cute_free(v);
+	v->type = b ? CUTE_TRUE : CUTE_FALSE;
+}
+//------get set boolean------
+
+//------get set string------
+void cute_set_string(cute_value* v, const char* s, size_t len)
+{
+	assert(v != NULL && (s != NULL || len == 0));
+	cute_free(v);
+	v->u.s.s = (char*)malloc(len + 1);
+	memcpy(v->u.s.s, s, len);
+	v->u.s.s[len] = '\0';
+	v->u.s.len = len;
+	v->type = CUTE_STRING;
+}
+
+const char* cute_get_string(const cute_value* v) {
+	assert(v != NULL && v->u.s.s != NULL && v->type == CUTE_STRING);
+	return v->u.s.s;
+}
+
+size_t cute_get_string_length(const cute_value* v) {
+	assert(v != NULL && v->u.s.s != NULL && v->type == CUTE_STRING);
+	return v->u.s.len;
+}
+//------get set string------
+
+//------parse value------
+static int cute_parse_null(cute_context* c, cute_value* v) {
+	EXPECT(c, 'n');
+	if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+		return CUTE_PARSE_INVALID_VALUE;
+	c->json += 3;
+	v->type = CUTE_NULL;
+	return CUTE_PARSE_OK;
+}
+
+static int cute_parse_true(cute_context* c, cute_value* v) {
+	EXPECT(c, 't');
+	if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+		return CUTE_PARSE_INVALID_VALUE;
+	c->json += 3;
+	v->type = CUTE_TRUE;
+	return CUTE_PARSE_OK;
+}
+
+static int cute_parse_false(cute_context* c, cute_value* v) {
+	EXPECT(c, 'f');
+	if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+		return CUTE_PARSE_INVALID_VALUE;
+	c->json += 4;
+	v->type = CUTE_FALSE;
+	return CUTE_PARSE_OK;
+}
+
+static int cute_parse_number(cute_context* c, cute_value* v)
+{
+	const char* p = c->json;
+	char* end;
+	/*负号 ... */
+	if (*p == '-') ++p;
+	/*整数 ... */
+	if (*p == '0') ++p;
+	else {
+		if (!ISDIGIT1TO9(*p)) return CUTE_PARSE_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
+	}
+	/*小数 ... */
+	if (*p == '.') {
+		++p;
+		if (!ISDIGIT(*p)) return CUTE_PARSE_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
+	}
+	/*指数 ... */
+	if (*p == 'e' || *p == 'E') {
+		++p;
+		if (*p == '+' || *p == '-') ++p;
+		if (!ISDIGIT(*p)) return CUTE_PARSE_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
+	}
+	errno = 0;//global scope variable
+	v->u.n = strtod(c->json, &end);
+	if (errno == ERANGE && v->u.n == HUGE_VAL) return CUTE_PARSE_NUMBER_TOO_BIG;
+	if (c->json == end)
+		return CUTE_PARSE_INVALID_VALUE;
+	c->json = end;
+	v->type = CUTE_NUMBER;
+	return CUTE_PARSE_OK;
+}
+
+static int cute_parse_literal(cute_context* c, cute_value* v, const char* literal, cute_type type) {
+	size_t i;
+	EXPECT(c, literal[0]);
+	for (i = 0; literal[i + 1]; ++i)
+		if (c->json[i] != literal[i + 1])
+			return CUTE_PARSE_INVALID_VALUE;
+	c->json += i;
+	v->type = type;
+	return CUTE_PARSE_OK;
+}
+
+//------parse string------
+#define PUTC(c, ch) do { *(char*)cute_context_push(c, sizeof(char)) = (ch); } while(0)
+
+static int cute_parse_string(cute_context* c, cute_value* v) {
+	size_t head = c->top, len;
+	const char* p;
+	EXPECT(c, '\"');
+	p = c->json;
+	for (;;) {
+		char ch = *p++;
+		switch (ch) {
+		case '\\':
+			switch (*p++) {
+				case '\"': PUTC(c, '\"'); break;
+				case '\\': PUTC(c, '\\'); break;
+				case '/':  PUTC(c, '/'); break;
+				case 'b':  PUTC(c, '\b'); break;
+				case 'f':  PUTC(c, '\f'); break;
+				case 'n':  PUTC(c, '\n'); break;
+				case 'r':  PUTC(c, '\r'); break;
+				case 't':  PUTC(c, '\t'); break;
+				default:
+				c->top = head;
+				return CUTE_PARSE_INVALID_STRING_ESCAPE;
+			}
+			break;
+		case '\"':
+			len = c->top = head;
+			cute_set_string(v, (const char*)cute_context_pop(c, len), len);
+			c->json = p;//reset
+			return CUTE_PARSE_OK;
+		case '\0':
+			c->top = head;
+			return CUTE_PARSE_MISS_QUOTATION_MARK;
+		default:
+			if ((unsigned char)ch < 0x20) {
+				c->top = head;
+				return CUTE_PARSE_INVALID_STRING_CHAR;
+			}
+			PUTC(c, ch);
+		}
+	}
+}
+//------parse string------
+
+static int cute_parse_value(cute_context* c, cute_value* v) {
+	switch (*c->json) {
+		case 'n': return cute_parse_null(c, v);		
+		case 't': return cute_parse_true(c, v);
+		case 'f': return cute_parse_false(c, v);
+		case '\"': return cute_parse_string(c, v);
+		default:  return cute_parse_number(c, v);
+		case '\0': return CUTE_PARSE_EXPECT_VALUE;
+	}
+}
+
+//------parse value------
+
+static void cute_parse_whitespace(cute_context* c) {
+	const char* p = c->json;
+	while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
+		++p;
+	c->json = p;
+}
+
+/*------JSON-text = ws value ws------*/
+int cute_parse(cute_value* v, const char* json) {
+	cute_context c;
+	int ret;
+	assert(v != NULL);
+	c.json = json;
+	c.stack = NULL;//<-
+	c.size = c.top = 0;//<-
+	cute_init(v);
+	v->type = CUTE_NULL;
+	//------trip white space------
+	cute_parse_whitespace(&c);
+	if ((ret = cute_parse_value(&c, v)) == CUTE_PARSE_OK)
+	{
+		cute_parse_whitespace(&c);
+		if (*c.json != '\0')
+			ret = CUTE_PARSE_ROOT_NOT_STNGULAR;
+	}
+	//------trip white space------
+	assert(c.top == 0);//<-
+	free(c.stack);//<-
+	return ret;
+}
--- CuteJson/CuteJson/main.cpp
+++ CuteJson/CuteJson/main.cpp
@@ -1,11 +1,124 @@
 #include <iostream>
+#include "cutejson.h"
 
-using namespace std;
+#ifdef _WINDOWS
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality, expect, actual, format) \
+    do {\
+        test_count++;\
+        if (equality)\
+            test_pass++;\
+        else {\
+            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+            main_ret = 1;\
+        }\
+    } while(0)
+
+#define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+#define EXPECT_TRUE(actual) EXPECT_EQ_BASE((1) == (actual), 1, actual, "%d")
+#define EXPECT_FALSE(actual) EXPECT_EQ_BASE((0) == (actual), 0, actual, "%d")
+
+#define TEST_NUMBER(expect, json)\
+    do{\
+        cute_value v;\
+        EXPECT_EQ_INT(CUTE_PARSE_OK, cute_parse(&v, json));\
+        EXPECT_EQ_INT(CUTE_NUMBER, cute_get_type(&v));\
+    }while(0)
+
+static void test_parse_null() {
+	cute_value v;
+	v.type = CUTE_TRUE;
+	EXPECT_EQ_INT(CUTE_PARSE_OK, cute_parse(&v, "null"));
+    EXPECT_EQ_INT(CUTE_PARSE_ROOT_NOT_STNGULAR, cute_parse(&v, "null x"));
+}
+
+static void test_parse_true() {
+    cute_value v;
+    v.type = CUTE_FALSE;
+    EXPECT_EQ_INT(CUTE_PARSE_OK, cute_parse(&v, "true"));
+    EXPECT_EQ_INT(CUTE_TRUE, cute_get_type(&v));
+}
+
+static void test_parse_false() {
+    cute_value v;
+    v.type = CUTE_TRUE;
+    EXPECT_EQ_INT(CUTE_PARSE_OK, cute_parse(&v, "false"));
+    EXPECT_EQ_INT(CUTE_FALSE, cute_get_type(&v));
+}
+
+static void test_parse_number() {
+	TEST_NUMBER(0.0, "0");
+	TEST_NUMBER(0.0, "-0");
+	TEST_NUMBER(0.0, "-0.0");
+	TEST_NUMBER(1.0, "1");
+	TEST_NUMBER(-1.0, "-1");
+	TEST_NUMBER(1.5, "1.5");
+	TEST_NUMBER(-1.5, "-1.5");
+	TEST_NUMBER(3.1416, "3.1416");
+	TEST_NUMBER(1E10, "1E10");
+	TEST_NUMBER(1e10, "1e10");
+	TEST_NUMBER(1E+10, "1E+10");
+	TEST_NUMBER(1E-10, "1E-10");
+	TEST_NUMBER(-1E10, "-1E10");
+	TEST_NUMBER(-1e10, "-1e10");
+	TEST_NUMBER(-1E+10, "-1E+10");
+	TEST_NUMBER(1.234E+10, "1.234E+10");
+	TEST_NUMBER(1.234E-10, "1.234E-10");
+	TEST_NUMBER(0.0, "1e-10000");
+}
+
+static void test_parse_string() {
+	cute_value v;
+    v.type = CUTE_STRING;
+    EXPECT_EQ_INT(CUTE_PARSE_OK, cute_parse(&v, "\"te\\bst\""));
+    EXPECT_EQ_INT(CUTE_STRING, cute_get_type(&v));
+}
+
+static void test_access_string() {
+    cute_value v;
+    cute_init(&v);
+    cute_set_string(&v, "", 0);
+    cute_set_string(&v, "Hello", 5);
+    cute_free(&v);
+}
+
+static void test_access_boolean() {
+    cute_value v;
+    cute_init(&v);
+    cute_set_string(&v, "a", 1);
+    cute_set_boolean(&v, 1);
+    EXPECT_TRUE(cute_get_boolean(&v));
+    cute_set_boolean(&v, 0);
+    EXPECT_FALSE(cute_get_boolean(&v));
+    cute_free(&v);
+}
+
+static void test_parse() {
+    test_parse_null();
+    test_parse_true();
+    test_parse_false();
+    test_parse_number();
+    test_parse_string();
+
+    //-----access------
+    test_access_string();
+    test_access_boolean();
+}
 
 int main()
 {
-	int32_t a = 5;
-
-	cout << a << endl;
+#ifdef _WINDOWS
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+    test_parse();
+    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+   
 	return 0;
 }
